44c1623179e5e41bdade32a3765969ccc80204a1
Tammy Pardo <ta.pardotroncoso@gmail.com>
2023-10-07 21:26:44 -0300

    Primer commit

---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import CartWidget from '../CartWidget/CartWidget'
+import "./NavBar.css"
+import imagen from "./logo.png"
+
+const NavBar = () => {
+  return (
+    <header className='navBar'>
+      <div className='brand'>
+        <img src={imagen} alt="Logo de la página" />
+        <h1>Libreria Koneko </h1>
+      </div>
+      <nav>
+        <ul className='categorias'>
+          <li>Libros</li>
+          <li>Mangas</li>
+          <li>Comics</li>
+          <li>Musica</li>
+          <li>Articulos</li>
+        </ul>
+      </nav>
+      <CartWidget/>
+    </header>
+  )
+}
+
+export default NavBar
